871a75ff6568297eb558c7cd288c4070d3b4f03c
Andrew Dushane <andrewdushane@gmail.com>
2016-01-25 13:18:37 -0600

    display images with message content

--- app_client/common/filters/prettifyMessage.filter.js
+++ app_client/common/filters/prettifyMessage.filter.js
@@ -13,17 +13,26 @@
         for(var i = 0; i < splitMessage.length; i++) {
           if(splitMessage[i].search(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/) != -1) {
             var linkText = splitMessage[i];
-            var linkUrl = splitMessage[i];
-            links.push(splitMessage[i]);
+            var linkUrl = linkText;
+            // Remove punctuation from end of link if present
+            var characters = ['.', ',', ';', ':', '?', '\\']
+            for(var j = 0; j < characters.length; j++) {
+              if(linkUrl.endsWith(characters[j])) {
+                var splitLink = linkUrl.split(characters[j]);
+                splitLink.pop();
+                linkUrl = splitLink.join(characters[j]);
+                break;
+              }
+            }
             // Add URL prefix if not present
             if(linkUrl.search(/^http.*$/) == -1) {
               linkUrl = 'http://' + linkUrl;
             }
             splitMessage[i] = '<a href="' + linkUrl + '" target="_blank">' + linkText + '</a>';
+            links.push(linkUrl);
           }
         }
         message = splitMessage.join(' ');
-        containsLink = true;
       }
       return {
         content: message,
@@ -31,15 +40,46 @@
       };
     }
 
-    // return filtered message
-    return function(message) {
+    function displayImages(message, links) {
+      var numLinks = links.length;
+      var imgTypes = ['jpg', 'png', 'gif']
+      for(var i = 0; i < numLinks; i++) {
+        var l = links[i];
+        if(l.endsWith('jpg') || l.endsWith('png') || l.endsWith('gif')) {
+          message += '<br><img src="' + l + '">';
+          links.splice(i, 1);
+        }
+      }
+      return {
+        content: message,
+        links: links
+      }
+    }
+
+    // Filter message
+    return function filterMessage(message) {
       var linked = linkify(message)
-      message = linked.content;
-      var links = linked.links;
+      var imaged = displayImages(linked.content, linked.links);
+      message = imaged.content;
+      var links = imaged.links;
       if(links) {
         console.log(links);
       }
       return message;
    };
+
+    // ES6 .endsWith polyfill
+    if (!String.prototype.endsWith) {
+      String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+          position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.indexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+      };
+    }
+
   }
 })();
