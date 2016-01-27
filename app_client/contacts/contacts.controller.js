daf99434be8324fe0bcb2a75ea432788d5339032
Andrew Dushane <andrewdushane@gmail.com>
2016-01-27 10:08:16 -0600

    welcome message for new registration

--- app_client/contacts/contacts.controller.js
+++ app_client/contacts/contacts.controller.js
@@ -8,9 +8,11 @@
     var vm = this;
 
     vm.accountid = localStorage.getItem('accountid');
+    vm.accountName = localStorage.getItem('accountName');
     vm.alertMessage = '';
     vm.apiUrl = constants.apiUrl;
     vm.contactsUrl = vm.apiUrl + '/account-contacts';
+    vm.hasContacts = true;
     // Get contact list for this account
     $http({
       method: 'GET',
@@ -21,12 +23,18 @@
     })
     .then(function successCallback(response) {
       vm.contacts = response.data;
+      if(vm.contacts && vm.contacts.length > 0) {
+        vm.hasContacts = true;
+      } else {
+        vm.hasContacts = false;
+      }
       for(var i= 0; i < vm.contacts.length; i++) {
         var id = vm.contacts[i].id
         vm.contacts[i].conversationUrl = '/account/' + vm.accountid + '/thread/' + vm.contacts[i].name + '/' + id
         var latest = vm.getLatestMessage(id, i);
       }
     }, function errorCallback(response) {
+      vm.hasContacts = false;
       console.log(response);
     });
 
@@ -60,6 +68,7 @@
       modalInstance.result.then(function(contact) {
         if(contact.id) {
           contact.conversationUrl = '/account/' + vm.accountid + '/thread/' + contact.name + '/' + contact.id;
+          vm.hasContacts = true;
           vm.contacts.push(contact);
         }
       });
