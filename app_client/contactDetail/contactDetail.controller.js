5c18dfcfba27ac318fb69b94d10d5d133661c783
Andrew Dushane <andrewdushane@gmail.com>
2016-01-28 13:14:50 -0600

    add profile image and delete button to contact view

--- app_client/contactDetail/contactDetail.controller.js
+++ app_client/contactDetail/contactDetail.controller.js
@@ -3,10 +3,11 @@
     .module('weMessageApp')
     .controller('contactDetailCtrl', contactDetailCtrl);
 
-    contactDetailCtrl.$inject = ['$scope', '$routeParams', '$http', 'constants'];
-    function contactDetailCtrl($scope, $routeParams, $http, constants) {
+    contactDetailCtrl.$inject = ['$scope', '$routeParams', '$http', '$location', 'constants'];
+    function contactDetailCtrl($scope, $routeParams, $http, $location, constants) {
       var vm = this;
       vm.detailUrl = constants.apiUrl + '/account/' + $routeParams.contactid;
+      vm.deleteUrl = constants.apiUrl + '/contacts/' + $routeParams.contactid;
       $http({
         method: 'GET',
         url: vm.detailUrl,
@@ -20,5 +21,32 @@
         console.log(response);
       });
       vm.onClickBack = constants.onClickBack;
+      vm.onClickDelete = function() {
+        $http({
+          method: 'DELETE',
+          url: vm.deleteUrl,
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+          }
+        })
+        .then(function successCallback(response) {
+          $location.path('/contacts');
+        }, function errorCallback(response) {
+          console.log('error');
+          console.log(response);
+        });
+        // $http({
+        //   method: 'DELETE',
+        //   url: vm.deletelUrl,
+        //   headers: {
+        //     'Authorization': 'Bearer ' + localStorage.getItem('authToken')
+        //   }
+        // })
+        // .then(function successCallback(response) {
+        //   $location.path('/contacts');
+        // }, function errorCallback(response) {
+        //   console.log('error');
+        // });
+      }
     }
 })();
