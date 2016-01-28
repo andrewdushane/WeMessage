caab1bb60f97e86f524207e0c9d27db0429a867d
Andrew Dushane <andrewdushane@gmail.com>
2016-01-28 17:02:45 -0600

    better nav links

--- app_client/common/directives/mainNavigation/mainNavigation.directive.js
+++ app_client/common/directives/mainNavigation/mainNavigation.directive.js
@@ -3,21 +3,37 @@
     .module('weMessageApp')
     .directive('mainNavigation', mainNavigation);
 
-  mainNavigation.$inject = ['$uibModal'];
-  function mainNavigation($uibModal) {
+  mainNavigation.$inject = ['$uibModal']
+  function mainNavigation($uibModal, scope) {
     var onClickLogout = function onClickLogout() {
-      console.log('whattup');
       var modalInstance = $uibModal.open({
         templateUrl: '/modals/logout/logout.view.html',
         controller: 'logoutCtrl as vm'
       });
+      modalInstance.result.then(function() {
+        console.log('then');
+      });
     }
+    var registrationPopup = function() {
+      var modalInstance = $uibModal.open({
+        templateUrl: '/modals/register/register.view.html',
+        controller: 'registerCtrl as vm'
+      });
+    };
+    var loginPopup = function() {
+      var modalInstance = $uibModal.open({
+        templateUrl: '/modals/login/login.view.html',
+        controller: 'loginCtrl as vm'
+      });
+    };
     return {
       restrict: 'EA',
       templateUrl: '/common/directives/mainNavigation/mainNavigation.template.html',
       link: function (scope) {
         scope.onClickLogout = onClickLogout;
         scope.loggedIn = localStorage.getItem('loggedIn');
+        scope.registrationPopup = registrationPopup;
+        scope.loginPopup = loginPopup;
       }
     };
   }
